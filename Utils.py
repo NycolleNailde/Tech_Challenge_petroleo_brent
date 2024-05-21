d57d0a24b06340973ff52fd5ea018df14baceffb
NycolleNailde <nycknailde@gmail.com>
2024-05-20 22:56:31 -0300

    Deploy dos modelos treinados e pipeline

--- Utils.py
+++ Utils.py
@@ -1,10 +1,17 @@
 
 import pandas as pd
+import numpy as np
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service # Classe Service
 from selenium.webdriver.common.by import By
-# from selenium.common.exceptions import WebDriverException
 
 def webscraping_ipea(url):
 
@@ -33,11 +40,11 @@ def webscraping_ipea(url):
             data, preco = row.split(" ")
             dados.append((data, float(preco.replace(',', '.'))))
 
-        new_df = pd.DataFrame(dados, columns=['Data', 'Preço - petróleo bruto - Brent (FOB)'])
-        new_df['Data'] = pd.to_datetime(new_df['Data'], format='%d/%m/%Y')
-        new_df = new_df.sort_values(by='Data',ascending=True)
+        df = pd.DataFrame(dados, columns=['Data', 'Preço - petróleo bruto - Brent (FOB)'])
+        df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y')
+        df = df.sort_values(by='Data',ascending=True)
 
-        return new_df
+        return df
 
     except:
         print('Falha ao acessar a página. Confira a url!')
@@ -70,6 +77,110 @@ def update_dataipea(url):
     else:
         print('Não atualizou!')
 
+def create_lag_feature(input):
+    # Criação de lag features
+    for lag in range(1, 3):  # Criar atrasos de 1 dia até 2 dias
+        input[f'Preço_lag_{lag}'] = input['Preço - petróleo bruto - Brent (FOB)'].shift(lag)
+
+    input = input.dropna()
+    x = input[['Preço_lag_1','Preço_lag_2']].values
+    return x
+
+def prev_week_gradient(modelo,x):
+    # Fazer previsões para a próxima semana usando os últimos dados conhecidos
+    input = create_lag_feature(x)
+
+    last_known_data = input[-1].reshape(1, -1)
+    last_data = x.iloc[-1:,0].iloc[0]
+    today = datetime.now()
+    
+    business_days = pd.date_range(start=last_data, end=today, freq='B')
+    h = len(business_days) + 5
+
+    next_week_predictions = []
+    for _ in range(h):  # para cada dia da próxima semana
+        next_day_pred = modelo.predict(last_known_data)[0]
+        next_week_predictions.append(next_day_pred)
+        last_known_data = np.roll(last_known_data, -1)
+        last_known_data[0, -1] = next_day_pred
+    
+    return next_week_predictions
+
+def plot_gradient(current_week_dates, current_week_prices,next_week_dates, next_week_predictions):
+    
+    current_week_dates = [date.strftime('%d/%m/%y') for date in current_week_dates]
+    next_week_dates = [date.strftime('%d/%m/%y') for date in next_week_dates]
+  
+    fig = make_subplots()
+    
+    # Adicionar trace dos preços atuais
+    fig.add_trace(go.Scatter(
+        x=current_week_dates,
+        y=current_week_prices,
+        mode='lines+markers',
+        name='Preços Reais',
+        line=dict(color='blue'),
+        marker=dict(color='blue')
+    ))
+
+    # Adicionar trace das previsões
+    fig.add_trace(go.Scatter(
+        x=next_week_dates,
+        y=next_week_predictions,
+        mode='lines+markers',
+        name='Previsões para a Próxima Semana',
+        line=dict(color='red', dash='dash'),
+        marker=dict(color='red')
+    ))
+
+    # Atualizar layout do gráfico
+    fig.update_layout(
+        # title='Histórico Recente Preço e Previsões de Preço para a Próxima Semana',
+        yaxis_title='Preço (US$)',
+        xaxis=dict(
+            tickangle=-45
+        ),
+        legend=dict(
+            x=0,
+            y=1,
+            traceorder='normal'
+        )
+    )
+
+    # Atualizar eixos x e y para exibir o gráfico corretamente
+    fig.update_xaxes(type='category')
+    fig.update_yaxes(type='linear')
+
+    data = {'Data': next_week_dates, 'Preço (US$)': next_week_predictions}
+    resultado = pd.DataFrame(data)
+
+    return [resultado, fig]   
+
+def prev_week_prophet(modelo):
+    last_date = datetime.strptime('2024-05-03', '%Y-%m-%d')
+
+    # Obter a data atual
+    today = datetime.now()
+
+    # Criar um range de datas úteis entre a data atual e a data específica
+    business_days = pd.date_range(start=last_date, end=today, freq='B')
+
+    # Calcular a quantidade de dias úteis e adicionar 5
+    h = len(business_days) + 5
+
+    future = modelo.make_future_dataframe(periods=h,freq='B')
+
+    forecast = modelo.predict(future)
+    fig_prophet = modelo.plot(forecast)
+
+    previsoes = forecast.rename(columns = {'ds':'Dia', 'yhat':'Preço (US$)'})
+    previsoes['Dia'] = previsoes['Dia'].dt.strftime('%d/%m/%y')
+    previsoes = previsoes[['Dia','Preço (US$)']].tail(h)
+    
+    return [previsoes, fig_prophet]
+
+def wmape(y_true, y_pred):
+  return np.abs(y_true-y_pred).sum() / np.abs(y_true).sum()
 
 
 
--- app.py
+++ app.py
@@ -1,45 +1,57 @@
 import pandas as pd
-import numpy as np
-from datetime import datetime
 
-from Utils import update_dataipea
-
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn.model_selection import train_test_split
-
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+from Utils import webscraping_ipea
+from Utils import create_lag_feature, prev_week_gradient,plot_gradient
+from Utils import prev_week_prophet
 
 import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-# import plotly.subplots as sp
-
-import seaborn as sns
-
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.stattools import adfuller
-from statsforecast import StatsForecast
-
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from statsforecast.models import AutoARIMA
-
-
 import streamlit as st
+import pickle
 
-# Chama função que atualiza base de dados
+# EXTRACT: 
+
+# webscraping dos dados mais recentes, para input dos modelos
 url = 'http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view'
-update_dataipea(url)
-# Lendo base
-df_ipea = pd.read_csv('./data/df_ipea.csv')
 
+df_ipea = webscraping_ipea(url)
+df_ipea['Data'] = pd.to_datetime(df_ipea['Data'],format='%Y-%m-%d')
+X_60= df_ipea.iloc[-60:,:]
+X_5= df_ipea.iloc[-5:,:]
+
+# Leitura dos artefatos com modelos treinados
+modelo_gradient = pickle.load(open('modelo_gradientboosting.pkl','rb'))
+modelo_prophet = pickle.load(open('modelo_prophet.pkl','rb'))
+
+# TRANSFORM: fazendo previsões
+
+# GradienteBoosting
+# input_grad = create_lag_feature(X_5)
+output_gr = prev_week_gradient(modelo_gradient,X_5)
+
+# FIGURA
+# As datas correspondentes à próxima semana
+h = len(output_gr)
+next_week_dates = pd.date_range(X_5['Data'].iloc[-1], periods=h+1,freq='B')[1:]
+next_week_dates = pd.to_datetime(next_week_dates,format='%Y-%m-%d')
+
+# Selecionar os dados da semana atual (últimos 7 dias do dataset)
+current_week_dates = X_60.iloc[-7:,0]
+current_week_prices = X_60.iloc[-7:,1]
+
+resultado_gr, fig_gradient = plot_gradient(current_week_dates, current_week_prices, next_week_dates, output_gr)
+
+
+# Prophet
+
+resultado_pr, fig_prophet= prev_week_prophet(modelo_prophet)
+
+
+# LOAD: streamlit
 # Título da Página
 st.title("Preço por barril do petróleo bruto Brent (FOB)")
 
 # Abas do app
-tab0, tab1, tab2, tab3 = st.tabs(['Geral','Previsões - Gradient Boosting', 'Previsões - AutoARIMA', 'Conclusão'])
+tab0, tab1, tab2= st.tabs(['Geral','Previsões - Prophet', 'Previsões - Gradient Boosting'])
 
 # Separando as Tabs
 with tab0:
@@ -47,70 +59,161 @@ with tab0:
     ## Petróleo Brent
     '''
     df = pd.DataFrame(df_ipea)
-    df = df.rename(columns={'Preço - petróleo bruto - Brent (FOB)':'Preço (U$)'})
-    st.dataframe(df,use_container_width=True)
+    df = df.rename(columns={'Preço - petróleo bruto - Brent (FOB)':'Preço (US$)'})
+    df_table = df.copy()
+    df_table['Data'] = df_table['Data'].dt.strftime('%d/%m/%y')
+    st.dataframe(df_table,use_container_width=True)
+    '''
+    Fonte: [IPEA](http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view).
+    '''
+    
 
-        # Plotar o gráfico de linha usando Plotly Express
-    fig = px.line(df, x='Data', y='Preço (U$)', title='Preço do Petróleo Brent ao longo do tempo')
+    # Plotar o gráfico de linha usando Plotly Express
+    fig = px.line(df, x='Data', y='Preço (US$)', title='Preço do Petróleo Brent ao longo do tempo')
 
     # Exibir o gráfico no Streamlit
     st.plotly_chart(fig)
     '''
 
-    ## Histórico
-    ### Ascensão do preço após 2004:
+    ## Análise do Histórico
+    ### Ascensão do preço após 2004
 
-    Crescimento econômico global:
     Durante esse período, houve um forte crescimento econômico global, especialmente nos países em 
-    desenvolvimento, como China, Índia e Brasil. O aumento da atividade econômica resultou em uma 
-    maior demanda por energia, incluindo petróleo, o que contribuiu para o aumento dos preços.
+    desenvolvimento, como China, Índia e Brasil. Com o aumento da demanda por petróleo, implusionado 
+    pelo rápido crescimento econômico em países emergentes, juntamente com o aumento da industrialização 
+    e urbanização, levou a um aumento significativo na demanda por petróleo. Isso colocou pressão adicional 
+    sobre os suprimentos globais e contribuiu para a alta dos preços.
 
-
-    Além disso, também houve o aumento da demanda por petróleo, implusionado pelo rápido crescimento 
-    econômico em países emergentes, juntamente com o aumento da industrialização e urbanização, levou 
-    a um aumento significativo na demanda por petróleo. Isso colocou pressão adicional sobre os 
-    suprimentos globais e contribuiu para a alta dos preços.
-
-    ### Queda em 2008:
+    ### Queda em 2008
 
     
     Um dos eventos mais significativos que impactou o preço do petróleo em 2008 foi a crise financeira global 
     que eclodiu na segunda metade daquele ano. A crise financeira teve origem nos Estados Unidos com a crise 
     do mercado imobiliário e a falência do banco de investimento Lehman Brothers em setembro de 2008.
 
-    A queda na demanda global por petróleo, combinada com preocupações sobre o excesso de oferta, levou a uma 
-    queda significativa nos preços do petróleo. No segundo semestre de 2008, o preço do petróleo bruto atingiu 
-    máximas históricas em torno de 'U$147' por barril, antes de cair drasticamente para menos de 'U$40' por barril no 
-    final do ano, refletindo a rápida deterioração das condições econômicas globais.
+    Essa crise espalhou-se pelo mundo, culminando em uma recessão global em 2009. Esse colapso financeiro levou 
+    à nacionalização de bancos, derrubou governos, gerou altas taxas de desemprego e desencadeou protestos. Esses
+     eventos fizeram com que o preço do petróleo Brent caísse drasticamente em 2008.
 
     ### Queda 2014
 
-    Após essa crise o preço se recuperou com certa estabilidade ente 2011 e 2014. Em 2014
+    Passada a crise de 2008, o preço se recuperou com certa estabilidade entre 2011 e 2014. Contudo, em 2014, o preço do petróleo 
+    Brent caiu devido a uma combinação de superprodução e demanda mais fraca do que o esperado na Europa e na 
+    Ásia. 
+    
+    O aumento da produção de petróleo de xisto nos EUA contribuiu significativamente para o excesso de 
+    oferta. Em novembro de 2014, a OPEP recusou-se a reduzir sua produção, exacerbando a queda dos preços, com a 
+    intenção de manter sua participação de mercado e inviabilizar a produção de rivais, especialmente produtores 
+    norte-americanos de petróleo de xisto.
 
     ### Queda 2020
 
-    ...
-
-    ### Alta em Março 2022
+    A pandemia de COVID-19 causou uma queda acentuada na demanda por petróleo devido ao isolamento social e à redução 
+    das atividades econômicas globais. Em março de 2020, a guerra de preços entre a Rússia e a Arábia Saudita 
+    também contribuiu para a queda vertiginosa dos preços. 
     
-    Base de Dados: 
-    
-    http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view
+    Em abril, o preço atingiu seu valor mínimo na história, devido à falta de armazenamento e compradores. Embora os 
+    preços tenham se recuperado posteriormente, eles não retornaram aos níveis pré-pandêmicos até 2022.
 
-    Links relevantes:
-    [1] https://brsa.org.br/wp-content/uploads/wpcf7-submissions/1016/1%C2%BA-arquivo.pdf
-    [2] https://g1.globo.com/economia/noticia/2015/01/entenda-queda-do-preco-do-petroleo-e-seus-efeitos.html
-    https://www.dw.com/pt-br/oito-motivos-para-a-queda-do-pre%C3%A7o-do-petr%C3%B3leo/a-19051686
-    https://www.cnnbrasil.com.br/economia/precos-de-petroleo-caem-de-maxima-de-2014-e-preocupacao-com-oferta-limita-perdas/
+    ## Metodologia
+
+    A série temporal dos preços do petróleo Brent é notoriamente volátil e suscetível a uma variedade de fatores externos, 
+    como crises financeiras, mudanças na produção global de petróleo e eventos geopolíticos significativos. Dada essa 
+    complexidade, a previsão precisa dos preços do petróleo requer modelos que possam capturar não apenas tendências 
+    e sazonalidades, mas também responder a variações abruptas e não lineares nos dados. Por isso, esse deploy apresenta
+    duas abordagens. Com uso dois modelos para a previsão desta série temporal: **Prophet** e **GradientBoostingRegressor**.
+
+    
+ 
+    Fontes:
+
+    https://economia.uol.com.br/noticias/bbc/2021/10/10/crise-financeira-colapso-que-ameacou-o-capitalismo.htm
+
+
+    https://g1.globo.com/economia/noticia/2015/01/entenda-queda-do-preco-do-petroleo-e-seus-efeitos.html
+
+    
     https://www.poder360.com.br/economia/pandemia-faz-preco-do-barril-de-petroleo-fechar-ano-20-mais-barato/#:~:text=O%20barril%20do%20petr%C3%B3leo%20encerrou,US%24%2066%2C00).
+
+
     https://economia.uol.com.br/noticias/afp/2020/12/31/o-preco-do-petroleo-fecha-2020-com-uma-queda-de-mais-de-20-devido-a-covid-19.htm
-    https://einvestidor.estadao.com.br/investimentos/preco-petroleo-2020/
-    https://www.cnnbrasil.com.br/economia/precos-do-petroleo-caem-us10-barril-em-maior-queda-diaria-desde-abril-de-2020/
-    https://oglobo.globo.com/economia/preco-do-petroleo-fecha-2020-com-uma-queda-de-mais-de-20-devido-pandemia-de-covid-19-1-24818960
-    https://g1.globo.com/economia/noticia/2020/03/09/o-que-explica-o-tombo-do-preco-do-petroleo-e-quais-os-seus-efeitos.ghtml
-
-
     
     '''
 
 
+with tab1:
+    '''
+    ## Prophet
+
+    O Prophet é uma ferramenta de previsão de séries temporais desenvolvida pelo Facebook. 
+    Ele é projetado para lidar com dados que possuem padrões sazonais e outliers, podendo 
+    assim capturar padrões sazonais anuais, mensais, semanais ou diários. Além disso, essa
+    ferramenta também é capaz de ajustar pontos de troca de tendência.
+
+
+    O modelo foi treinado com dados, disponibilizados pelo [IPEA](http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view), 
+    entre período entre 08/02/2024 até 03/05/2024, com sazonalidade semanal. 
+    ```
+    modelo = Prophet()
+    modelo.add_seasonality(name='weekly', period=2, fourier_order=5)
+    modelo.fit(treino.iloc[-60:,:])
+    ```
+    '''
+    st.markdown('#### Histórico e Previsão de Preços do Petróleo Bruto')
+    # fig_prophet.suptitle('Histórico e Previsão de Preços do Petróleo Bruto')
+    fig_prophet.axes[0].set_xlabel('Data')
+    fig_prophet.axes[0].set_ylabel('Preço (US$)')
+    st.pyplot(fig_prophet, use_container_width=True)
+
+    st.markdown('### Previsões')
+    st.table(resultado_pr)
+    '''
+    ### Métricas de validação
+
+    As métricas foram calculadas pelo conjunto de teste com dados do período 07/05/2024 a 13/05/2024.
+
+    WMAPE: 1.38%  
+    Mean Squared Error: 1.83  
+    Mean Absolute Error: 1.15
+
+    Vale ressaltar que essas métricas se alteram na medida em que os dados previstos estão mais distantes dos dados de treino.
+    '''
+
+with tab2:
+    '''
+    ## Gradient Boosting
+
+    
+    O GradientBoostingRegressor é um modelo de Machine Learning, baseado em árvores de decisão, que é usado para resolver problemas de regressão. 
+    Ele pertence à família de modelos de boosting, que trabalham construindo uma sequência de modelos de forma iterativa, onde cada modelo 
+    subsequente tenta corrigir os erros do modelo anterior. É adequado para capturar relações complexas e não lineares nos dados.
+
+    O modelo foi treinado com toda a série histórica, disponibilizada pelo [IPEA](http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view) 
+    até o dia 17/05/2024. Ou seja, período compreendido entre 20/05/1987 até 13/05/2024. 
+
+    ```
+    modelo = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=5, random_state=42, loss='squared_error')
+    model.fit(X_train, y_train)
+    ```
+    
+    '''
+    st.markdown('#### Histórico Recente Preço e Previsões de Preço para a Próxima Semana')
+    st.plotly_chart(fig_gradient, use_container_width=True)
+
+    st.markdown('### Previsões')
+    st.table(resultado_gr)
+
+    '''
+    ### Métricas de validação:
+
+    As métricas foram calculadas para todo conjunto de treino, com dados do período de 20/05/1987 até 13/05/2024.
+
+    WMAPE: 1.68%
+    Mean Squared Error: 2.80
+    Mean Absolute Error: 1.18
+
+    Apesar de o Prophet apresentar métricas de desempenho superiores, o modelo GradientBoostingRegressor se destaca por sua robustez. 
+    Isso se deve ao fato de que o GradientBoostingRegressor foi treinado com todos os dados históricos disponíveis da série temporal. 
+    Ao utilizar o conhecimento adquirido durante o treinamento, o modelo faz previsões com base nas lag features dos dados mais recentes, 
+    evitando assim o problema que pode ocorrer com o Prophet à medida que as previsões se distanciam dos dados de treinamento.
+    '''
